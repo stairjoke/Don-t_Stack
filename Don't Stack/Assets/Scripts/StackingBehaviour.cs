02c06a36cb7da1c7871ebd856c3f903fb75fbe70
Wenzel Massag <mail@wmassag.de>
2017-12-02 10:34:07 +0100

    Collision detection in stack

---
+++ Don't Stack/Assets/Scripts/StackingBehaviour.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace gameJam
+{
+    public class StackingBehaviour : MonoBehaviour
+    {
+
+        public Transform above;
+        public Transform below;
+        public Transform body;
+
+        void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.transform.GetComponent<StackingBehaviour>())
+            {
+                switch (other.transform.GetComponent<StackingBehaviour>().body.GetComponent<Type>().myType)
+                {
+                    case blockTypes.undefined:
+                        undefinedCollision();
+                        break;
+                    case blockTypes.Apfel:
+                        apfelCollision();
+                        break;
+                    case blockTypes.Erdbeere:
+                        erdbeereCollision();
+                        break;
+                    case blockTypes.Marshmallow:
+                        marshmallowCollision();
+                        break;
+                }
+            }
+        }
+
+        private void undefinedCollision(){
+            Debug.Log("undefined");
+        }
+        private void apfelCollision() {
+            Debug.Log("Apfel");
+        }
+        private void erdbeereCollision() {
+            Debug.Log("Erdbeere");
+        }
+        private void marshmallowCollision() {
+            Debug.Log("Marshmallow");
+        }
+    }
+}
