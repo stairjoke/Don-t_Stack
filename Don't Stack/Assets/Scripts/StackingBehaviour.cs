61f0f82ec2960d2425aab9d1df1523b2cad5815d
Wenzel Massag <mail@wmassag.de>
2017-12-02 16:13:01 +0100

    Mega zu viel

--- Don't Stack/Assets/Scripts/StackingBehaviour.cs
+++ Don't Stack/Assets/Scripts/StackingBehaviour.cs
@@ -6,11 +6,7 @@ namespace gameJam
 {
     public class StackingBehaviour : MonoBehaviour
     {
-
-        public Transform above;
-        public Transform below;
         public Transform body;
-        public Sprite effect;
         private bool hasStacked = false;
 
 
@@ -33,43 +29,23 @@ namespace gameJam
                 case blockTypes.Marshmallow:
                     marshmallowCollision();
                     break;
+                case blockTypes.Wurst:
+                    wurstCollision();
+                    break;
             }
         }
 
-        /*
-        void OnCollisionEnter2D(Collision2D other)
-        {
-            if (other.transform.GetComponent<StackingBehaviour>())
-            {
-                switch (other.transform.GetComponent<StackingBehaviour>().body.GetComponent<Type>().myType)
-                {
-                    case blockTypes.undefined:
-                        undefinedCollision();
-                        break;
-                    case blockTypes.Apfel:
-                        apfelCollision();
-                        break;
-                    case blockTypes.Erdbeere:
-                        erdbeereCollision();
-                        break;
-                    case blockTypes.Marshmallow:
-                        marshmallowCollision();
-                        break;
-                }
-            }
-        }
-*/
         private void undefinedCollision(){
             hasStacked = true;
         }
         private void apfelCollision() {
             Debug.Log("Apfel");
             if(!hasStacked){
-                body.GetComponent<SpriteRenderer>().sprite = effect;
+                body.GetComponent<Animator>().SetBool("bounce", true);
                 GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 10f), ForceMode2D.Impulse);
-                GetComponent<Rigidbody2D>().AddTorque(Mathf.Ceil(Random.Range(0f, 1f) - 0.5f) * 20f, ForceMode2D.Impulse);
+                GetComponent<Rigidbody2D>().AddTorque(Mathf.Ceil(Random.Range(0f, 1f) - 0.5f) * 5f, ForceMode2D.Impulse);
+                hasStacked = true;
             }
-            hasStacked = true;
         }
         private void erdbeereCollision() {
             Debug.Log("Erdbeere");
@@ -78,8 +54,19 @@ namespace gameJam
         }
         private void marshmallowCollision() {
             Debug.Log("Marshmallow");
-
-            hasStacked = true;
+            if (!hasStacked)
+            {
+                body.GetComponent<Animator>().SetBool("smash", true);
+                hasStacked = true;
+            }
+        }
+        private void wurstCollision()
+        {
+            Debug.Log("Wurst");
+            if(!hasStacked){
+                body.GetComponent<Animator>().SetBool("splash", true);
+                hasStacked = true;
+            }
         }
     }
 }
