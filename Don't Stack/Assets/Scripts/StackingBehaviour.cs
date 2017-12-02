6ced453910f582ca78babeb8d7ba3a5064474935
Wenzel Massag <mail@wmassag.de>
2017-12-02 22:00:32 +0100

    Sounds

--- Don't Stack/Assets/Scripts/StackingBehaviour.cs
+++ Don't Stack/Assets/Scripts/StackingBehaviour.cs
@@ -48,6 +48,7 @@ namespace gameJam
         private void apfelCollision() {
             Debug.Log("Apfel");
             if(!hasStacked){
+                GetComponent<AudioSource>().Play();
                 body.GetComponent<Animator>().SetBool("bounce", true);
                 GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 10f), ForceMode2D.Impulse);
                 GetComponent<Rigidbody2D>().AddTorque(Mathf.Ceil(Random.Range(0f, 1f) - 0.5f) * 5f, ForceMode2D.Impulse);
@@ -91,6 +92,7 @@ namespace gameJam
             {
                 body.GetComponent<Animator>().SetBool("splash", true);
                 Camera.main.GetComponent<Score>().updateScore(50);
+                GetComponent<AudioSource>().Play();
                 hasStacked = true;
             }
         }
