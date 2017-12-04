0459eb1568a9eca5dd5b3942bff5b37339bed6e6
Wenzel Massag <mail@wmassag.de>
2017-12-04 12:00:06 +0100

    Done

---
+++ Don't Stack/Assets/Scripts/Exit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Exit : MonoBehaviour {
+    public void mainMenu(){
+        SceneManager.LoadScene(0);
+    }
+    public void exit(){
+        Debug.Log("EXIT APP");
+        Application.Quit();
+    }
+}
